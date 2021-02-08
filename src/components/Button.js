6c96e8cf5ad1316daabdd0b85f34a8a2894f26de
04alexklink <alex_klink123@hotmail.co.uk>
2021-02-08 19:55:18 +0000

    Added a onClick prop to Button component that takes a function as a parameter that can be used as the ballback function in the Button onclick listener. In Header component, created a specific onClick function that is passed as an argument to the Button component, so a specialised onClick event for this button is created

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,9 +1,21 @@
-const Button = ({ color, text }) => {
+import PropTypes from 'prop-types';
+
+const Button = ({ color, text, onClick }) => {
   return (
-    <button className="btn" style = {{backgroundColor: color}}>
+    <button onClick = {onClick} className="btn" style = {{backgroundColor: color}}>
       {text}
     </button>
   )
 }
 
+Button.defaultProps = {
+  color: "black",
+  // could set a default onClick here if one wasn't passed in as an argument
+}
+
+Button.propTypes = {
+  color: PropTypes.string,
+  text: PropTypes.string,
+  onClick: PropTypes.func
+}
 export default Button
