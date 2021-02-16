0dabc4ee1a1b59fc613a8c4b2a749269b5fe445c
04alexklink <alex_klink123@hotmail.co.uk>
2021-02-16 17:07:34 +0000

    Task toggle reminder. If double click on a task, the reminder value in that tasks state is set to the reverse of what it currently is. When task created, now checks if task.reminder is set to true, and if it is, adds className reminder, linked to CSS class that makes the task have a green LHS, acting as a reminder

--- src/App.js
+++ src/App.js
@@ -32,10 +32,15 @@ const deleteTask = (id) => {
   }))
 }
 
+const toggleReminder = (id) => {
+  setTasks(tasks.map((task) => task.id === id ?
+  {...task, reminder: !task.reminder} : task))
+}
+
   return (
     <div className="container">
       <Header title="Task Tracker"/>
-      {tasks.length > 0 ? <Tasks tasks={tasks} onDelete={deleteTask}/> : "No tasks to show"}
+      {tasks.length > 0 ? <Tasks tasks={tasks} onDelete={deleteTask} onToggleReminder={toggleReminder}/> : "No tasks to show"}
     </div>
   );
 }
