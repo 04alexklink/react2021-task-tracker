eb6ccb3fbc5db8f7c0cbc9d3febd0029ef533693
04alexklink <alex_klink123@hotmail.co.uk>
2021-02-16 22:13:07 +0000

    JSON server used to mock interaction with a backend/replicate this interaction. Inserted tasks JSON into db.json which runs on localserver:5000. Added useEffect to App component and created fetchData function that uses the fetch API to get tasks data at localhost:5000/tasks

--- src/App.js
+++ src/App.js
@@ -1,30 +1,18 @@
 import Header from './components/Header'
 import Tasks from './components/Tasks'
 import AddTask from './components/AddTask'
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 function App() {
   const [showAddTask, setShowAddTask] = useState(false)
-  const [tasks, setTasks] = useState([
-    {
-      id:1,
-      text: 'Doctors Appointment',
-      day: 'Feb 5th at 2:30pm',
-      reminder: true
-  },
-  {
-    id:2,
-    text: 'Meeting at School',
-    day: 'Feb 6th at 1:30pm',
-    reminder:true
-  },
-  {
-    id:3,
-    text: 'Food Shopping',
-    day: 'Feb 5th at 2:30pm',
-    reminder: false
+  const [tasks, setTasks] = useState([])
+  useEffect(() => {
+  const fetchTasks = async() => {
+    const response = await fetch('http://localhost:5000/tasks')
+    const data = await response.json()
+    console.log(data)
   }
-]
-)
+  fetchTasks()
+  }, [])
 
 const addTask = (task) => {
   const id = Math.floor(Math.random() * 10000) + 1
