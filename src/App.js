04e4bd3999430badbffd149548f6b49fd09a070b
04alexklink <alex_klink123@hotmail.co.uk>
2021-02-16 16:04:42 +0000

    If no tasks in state, output 'No tasks to show' message

--- src/App.js
+++ src/App.js
@@ -35,7 +35,7 @@ const deleteTask = (id) => {
   return (
     <div className="container">
       <Header title="Task Tracker"/>
-      <Tasks tasks={tasks} onDelete={deleteTask}/>
+      {tasks.length > 0 ? <Tasks tasks={tasks} onDelete={deleteTask}/> : "No tasks to show"}
     </div>
   );
 }
--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -1,6 +1,6 @@
 import Task from './Task';
 
-const Tasks = ( { tasks, onDelete}) => {
+const Tasks = ( { tasks, onDelete }) => {
   return (
     <>
     {tasks.map((task) => (
