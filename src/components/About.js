220c8f347f3f6578b250bd0b490c9f8a5bbf9182
04alexklink <alex_klink123@hotmail.co.uk>
2021-02-17 11:33:59 +0000

    Imported Link from react-router-dom to use in place of a tags in Footer and About components. This ensures the page doesn't reload when clicking on a link

--- src/components/About.js
+++ src/components/About.js
@@ -1,8 +1,9 @@
+import { Link } from 'react-router-dom'
 const About = () => {
  return (
      <div>
      <h1>Hello it's me, the About page</h1>
-     <a href="/">Go back to tasks</a>
+     <Link to='/'>Go back to tasks</Link>
      </div>
  )
 }
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,9 @@
-import Button from './Button'
+import { Link } from 'react-router-dom'
  const Footer = () => {
 
   return (
       <footer className="footer">
-          <a href='/about'>About</a>
+          <Link to='/about'>About</Link>
       </footer>
   )
 }
